3f67cd6bd6ad0024464a7f58ef5d9407631aadc2
Firestack <me@stackfire.io>
2016-05-24 21:08:26 -0600

    Change names for merge

---
+++ BotVentic/BotVentic/Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Configuration.Install;
+using System.Configuration.Assemblies;
+
+namespace BotVenticService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                        Console.WriteLine("Installing service...");
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        Console.WriteLine("Uninstalling service...");
+                        break;
+
+                    default:
+                        BotVentic.DiscordBot.Mainloop();
+                        break;
+                }
+            }
+            else
+            {
+
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+
+                {
+                    new BotVentic.BDService()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+
+        }
+    }
+}
